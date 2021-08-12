398be6ef8b5de80442db5bd6b9d102e5ec8600db
Michael De Luca <35537333+deluca-mike@users.noreply.github.com>
2021-08-12 15:33:39 -0400

    feat: Better Make Release (#4)
    - and take latest subfactory

--- release.sh
+++ release.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-while getopts v: flag
-do
-    case "${flag}" in
-        v) version=${OPTARG};;
-    esac
-done
-
-echo $version
+version=$(cat ./package.yaml | grep "version: " | sed -r 's/.{9}//')
+name=$(cat ./package.yaml | grep "name: " | sed -r 's/.{6}//')
+customDescription=$(cat ./package.yaml | grep "customDescription: " | sed -r 's/.{19}//')
 
 ./build.sh -c ./config/prod.json
 
@@ -16,28 +11,31 @@ rm -rf ./package
 mkdir -p package
 
 echo "{
-  \"name\": \"@maplelabs/debt-locker\",
+  \"name\": \"@maplelabs/${name}\",
   \"version\": \"${version}\",
-  \"description\": \"Debt Locker Artifacts and ABIs\",
+  \"description\": \"${customDescription}\",
   \"author\": \"Maple Labs\",
   \"license\": \"AGPLv3\",
   \"repository\": {
     \"type\": \"git\",
-    \"url\": \"https://github.com/maple-labs/debt-locker.git\"
+    \"url\": \"https://github.com/maple-labs/${name}.git\"
   },
   \"bugs\": {
-    \"url\": \"https://github.com/maple-labs/debt-locker/issues\"
+    \"url\": \"https://github.com/maple-labs/${name}/issues\"
   },
-  \"homepage\": \"https://github.com/maple-labs/debt-locker\"
+  \"homepage\": \"https://github.com/maple-labs/${name}\"
 }" > package/package.json
 
 mkdir -p package/artifacts
 mkdir -p package/abis
 
-cat ./out/dapp.sol.json | jq '.contracts | ."contracts/DebtLockerFactory.sol" | .DebtLockerFactory' > package/artifacts/DebtLockerFactory.json
-cat ./out/dapp.sol.json | jq '.contracts | ."contracts/DebtLockerFactory.sol" | .DebtLockerFactory | .abi' > package/abis/DebtLockerFactory.json
-cat ./out/dapp.sol.json | jq '.contracts | ."contracts/DebtLocker.sol" | .DebtLocker' > package/artifacts/DebtLocker.json
-cat ./out/dapp.sol.json | jq '.contracts | ."contracts/DebtLocker.sol" | .DebtLocker | .abi' > package/abis/DebtLocker.json
+paths=($(cat ./package.yaml | grep "  - path:" | sed -r 's/.{10}//'))
+names=($(cat ./package.yaml | grep "    contractName:" | sed -r 's/.{18}//'))
+
+for i in "${!paths[@]}"; do
+  cat ./out/dapp.sol.json | jq ".contracts | .\"${paths[i]}\" | .${names[i]}" > package/artifacts/${names[i]}.json
+  cat ./out/dapp.sol.json | jq ".contracts | .\"${paths[i]}\" | .${names[i]} | .abi" > package/abis/${names[i]}.json
+done
 
 npm publish ./package --access public
 
